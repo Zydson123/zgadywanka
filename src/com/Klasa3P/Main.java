bb743c076acc0b73c933a598dfd76fe045d662e4
zydson123 <mlgplaygaming@gmail.com>
2022-09-08 11:41:47 +0200

    do while

--- src/com/Klasa3P/Main.java
+++ src/com/Klasa3P/Main.java
@@ -6,38 +6,30 @@ public class Main {
     public static void main(String[] args) {
         //progarm zgadnij liczbę
         int losuj = (int) (Math.random() * 100 + 1);
-        int ile=10;
+        int ile = 10;
         boolean czy = false;
-        while(!czy) {
-            if(ile==0){
-                break;
-            }
-            Scanner wpisz = new Scanner(System.in);
-            System.out.println("Zgadnij liczbę");
-            int wpisana = wpisz.nextInt();
-            if (wpisana == losuj) {
-                ile--;
-                int ktoryraz = 10-ile;
-                System.out.println("Gratulacje! Udało ci się za " + ktoryraz + " razem");
-                czy = true;
-            }
-            else{
-                if(wpisana>losuj){
+        System.out.println("Zgadnij liczbę");
+        do {
+                System.out.println("Podaj liczbę");
+                Scanner wpisz = new Scanner(System.in);
+                int wpisana = wpisz.nextInt();
+                if(wpisana == losuj){
+                    break;
+                }
+                if (wpisana > losuj) {
                     System.out.println("mniej");
                     ile--;
+                    System.out.println("Pozostało " + ile + " prób");
                 }
-                else{
+                else {
                     System.out.println("więcej");
                     ile--;
+                    System.out.println("Pozostało " + ile + " prób");
                 }
             }
-        if(!czy && ile==0){
-            System.out.println("Nie udało się, nie masz już prób");
-            System.out.println("Liczbą to była: " + losuj);
-        }
-        else if(!czy){
-            System.out.println("Nie udało się, masz jeszcze " + ile + " prób");
-        }
+            while (true);
+            ile--;
+            int zaktorym = 10-ile;
+            System.out.println("gratulacje! udało ci się za " + zaktorym + " razem");
         }
     }
-}
